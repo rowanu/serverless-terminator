3127a32b41673a7665d8bfad8d655329872277c0
rowan <rowanu@gmail.com>
2016-11-02 20:57:47 +1100

    Add basic terminator function.

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-'use strict';
+const AWS = require('aws-sdk');
 
 module.exports.hello = (event, context, callback) => {
   const response = {
@@ -13,4 +13,16 @@ module.exports.hello = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+const ec2 = new AWS.EC2();
+
+const describeInstances = () =>
+  ec2.describeInstances().promise();
+
+module.exports.terminator = (event, context, callback) => {
+  Promise.resolve(event)
+    .then(describeInstances)
+    .then(data => console.log(JSON.stringify(data, null, 2)))
+    .catch(callback);
 };
