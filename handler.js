f6232b294c3636bb343ce1bc465be11fe76e932e
rowan <rowanu@gmail.com>
2016-11-02 22:15:53 +1100

    Comments and whitespace changes.

--- handler.js
+++ handler.js
@@ -5,9 +5,8 @@ const ec2 = new AWS.EC2();
 // Returns an array of all instances.
 const describeInstances = () =>
   ec2.describeInstances({ Filters: [{ Name: 'instance-state-name', Values: ['running'] }] }).promise()
-    .then(data => data.Reservations.map(r => r.Instances)) // Flatten instances
+    .then(data => data.Reservations.map(r => r.Instances))
     .then(instances => instances.reduce((a, b) => a.concat(b), []));
-
 
 // Returns true if instance.tags includes a Key with 'Name'.
 const instanceHasNoNameTag = instance => instance.Tags.filter(t => t.Key === 'Name').length < 1;
@@ -18,6 +17,7 @@ const filterInvalidInstances = (instances) => {
   return instancesMissingNameTag;
 };
 
+// Output invalid instances to the console/CloudWatch Logs.
 const report = (invalidInstances) => {
   console.log(`The following ${invalidInstances.length} instances are invalid, and will be terminated:\n`,
     JSON.stringify(invalidInstances, null, 2));
