8a32f3b507e5d7f9bd1f5fb1b2191a2669f7c17a
rowan <rowanu@gmail.com>
2016-11-02 21:47:00 +1100

    Remove default hello function.

--- handler.js
+++ handler.js
@@ -1,20 +1,5 @@
 const AWS = require('aws-sdk');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 const ec2 = new AWS.EC2();
 
 // Returns an array of all instances.
