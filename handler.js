2fac400c48b9b4d1ecbbf4d7abaa507b0704df77
rowan <rowanu@gmail.com>
2016-11-03 11:57:25 +1100

    Extract instance ids to simplify reporting and termination.

--- handler.js
+++ handler.js
@@ -9,7 +9,7 @@ const describeInstances = () =>
     .then(instances => instances.reduce((a, b) => a.concat(b), []));
 
 // Returns true if instance.tags includes a Key with 'Name'.
-const instanceHasNoNameTag = instance => instance.Tags.filter(t => t.Key === 'Name').length < 1;
+const instanceHasNoNameTag = instance => instance.Tags.filter(t => t.Key === 'Name').length === 0;
 
 // Returns an array of invalid instances.
 const filterInvalidInstances = (instances) => {
@@ -17,21 +17,25 @@ const filterInvalidInstances = (instances) => {
   return instancesMissingNameTag;
 };
 
+// Get just the instance ids.
+const getInstanceIds = instances => instances.map(i => i.InstanceId);
+
 // Output invalid instances to the console/CloudWatch Logs.
-const report = (invalidInstances) => {
-  console.log(`The following ${invalidInstances.length} instances are invalid, and will be terminated:\n`,
-    JSON.stringify(invalidInstances, null, 2));
-  return invalidInstances;
+const report = (instanceIds) => {
+  console.log(`Found ${instanceIds.length} invalid instances to terminate ${instanceIds}`);
+  return instanceIds;
 };
 
 // Terminate instances.
-const terminateInstances = instances =>
-  ec2.terminateInstances({ InstanceIds: instances.map(i => i.InstanceId) }).promise();
+const terminateInstances = (instanceIds) => {
+  if (instanceIds.length) ec2.terminateInstances({ InstanceIds: instanceIds }).promise();
+};
 
 module.exports.terminator = (event, context, callback) => {
   Promise.resolve(event)
     .then(describeInstances)
     .then(filterInvalidInstances)
+    .then(getInstanceIds)
     .then(report)
     .then(terminateInstances)
     .catch(callback);
